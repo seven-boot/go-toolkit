394cd5d46bbab419243858a081eb08cd47746e80
seven <1042899106@qq.com>
2021-04-29 10:09:15 +0800

    add gin,result

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "go-toolkit"
+module github.com/seven-boot/go-toolkit
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.7.1 // indirect
---
+++ result.go
@@ -0,0 +1,47 @@
+package toolkit
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	SUCCESS int = 99999
+	FAILED  int = 10001
+)
+
+// 数据交互对象
+type Result struct {
+	Code int         `json:"code"`
+	Data interface{} `json:"data"`
+	Msg  string      `json:"msg"`
+}
+
+func FastSuccess(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, Result{
+		Code: SUCCESS,
+		Msg:  "成功",
+	})
+}
+
+func FastFailed(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, Result{
+		Code: FAILED,
+		Msg:  "请求失败",
+	})
+}
+
+func Success(ctx *gin.Context, v interface{}) {
+	ctx.JSON(http.StatusOK, Result{
+		Code: SUCCESS,
+		Msg:  "成功",
+		Data: v,
+	})
+}
+
+func Failed(ctx *gin.Context, s string) {
+	ctx.JSON(http.StatusOK, Result{
+		Code: FAILED,
+		Msg:  s,
+	})
+}
